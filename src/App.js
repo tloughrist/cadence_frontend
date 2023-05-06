5bf96e01b075d4b1824da55822b651d92bc6b7c9
timloughrist <tim.loughrist@gmail.com>
2023-05-06 07:37:33 -0500

    build cadence card

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
+import React, { useState } from "react";
 import './input.css';
 
 function App() {
+
+  const [spm, setSPM] = useState(0);
+  const [genre, setGenre] = useState("");
+
   return (
     <div>
       <div class="p-6 max-w-sm mx-auto mt-32 bg-white rounded-xl shadow-lg flex items-center space-x-4">
         <div>
           <div class="text-xl font-medium text-black">Cadence</div>
-          <p class="text-slate-500">Select your cadence</p>
         </div>
+        <form>
+          <label htmlFor="strides">
+            strides per minute
+            <input
+              type="text"
+              name="spm"
+              value={spm}
+              onChange={(e) => setSPM(e.target.value)}
+            />
+          </label>
+          <label htmlFor="genre">
+            genre of music
+            <select
+              name="genre"
+              value={genre}
+              onChange={(e) => setGenre(e.target.value)}
+            >
+              <option value={""}>Any</option>
+              <option value={"rock"}>Rock</option>
+              <option value={"edm"}>EDM</option>
+              <option value={"hiphop"}>Hip Hop</option>
+            </select>
+          </label>
+          <button className="navlink" type="submit">Find songs</button>
+        </form>
       </div>
     </div>
   );
