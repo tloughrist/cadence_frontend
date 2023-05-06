1ddca53dab2fd701fa17fd41a9f7356d1f5721fd
timloughrist <tim.loughrist@gmail.com>
2023-05-06 07:24:02 -0500

    experiment with tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import './input.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div class="p-6 max-w-sm mx-auto mt-32 bg-white rounded-xl shadow-lg flex items-center space-x-4">
+        <div>
+          <div class="text-xl font-medium text-black">Cadence</div>
+          <p class="text-slate-500">Select your cadence</p>
+        </div>
+      </div>
     </div>
   );
 }
