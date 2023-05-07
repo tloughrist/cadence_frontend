35613ba0aaaf34f8d3b97c5eacd3ba64a90e841c
timloughrist <tim.loughrist@gmail.com>
2023-05-07 10:18:01 -0500

    try different endpoint

--- src/fetching/fetch_genres.js
+++ src/fetching/fetch_genres.js
@@ -1,11 +1,10 @@
 export default async function fetchGenres(token) {
-  const res = await fetch("https://accounts.spotify.com/api/recommendations/available-genre-seeds", {
+  const res = await fetch("https://api.spotify.com/v1/artists/4Z8W4fKeB5YxbusRsdQVPb", {
     method: "Get",
     headers: {
       'Authorization': `Bearer ${token}`
     }
   });
   const genres = await res.json();
-  console.log(genres);
   return genres;
 };
