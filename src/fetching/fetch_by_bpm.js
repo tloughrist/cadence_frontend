b8b09f3b0eae5dc5f3197b855ce236c553c32abb
timloughrist <tim.loughrist@gmail.com>
2023-05-12 04:17:29 -0500

    fix fetching

--- src/fetching/fetch_by_bpm.js
+++ src/fetching/fetch_by_bpm.js
@@ -1,6 +1,6 @@
 export default async function fetchByBPM(low, high) {
 
-  const res = await fetch(`tracks/bpm/${low}/${high}`);
+  const res = await fetch(`https://cadence-h597.onrender.com/tracks/bpm/${low}/${high}`);
   if (res.statusText === "No Content") {
     return "error";
   } else {
