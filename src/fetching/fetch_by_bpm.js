8075153fc74f9fac4de7e922c8f3cbe7740f2d39
timloughrist <tim.loughrist@gmail.com>
2023-05-12 05:20:08 -0500

    add status_text log

--- src/fetching/fetch_by_bpm.js
+++ src/fetching/fetch_by_bpm.js
@@ -1,6 +1,7 @@
 export default async function fetchByBPM(low, high) {
 
   const res = await fetch(`https://cadence-h597.onrender.com/tracks/bpm/${low}/${high}`);
+  console.log (res.statusText)
   if (res.statusText === "No Content") {
     return "error";
   } else {
